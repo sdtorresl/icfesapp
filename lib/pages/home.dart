52b4ea4f6d0f1b420e81ff95fc5c7442477ba25d
Sergio Torres <sdtorresl@unal.edu.co>
2020-09-08 21:50:25 -0500

    Designed top bar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
@@ -6,11 +7,49 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("IFCES App"),
-      ),
+      appBar: _topBar(context),
       body: Container(
         child: Text("Hola mundo cruel"),
+      ),
+      //bottomNavigationBar: BottomNavigationBar(items: null),
+    );
+  }
+
+  Widget _topBar(context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double barHeight = 90;
+
+    return PreferredSize(
+      preferredSize: Size(screenWidth, barHeight),
+      child: Container(
+        child: Stack(
+          children: [
+            Container(
+              padding: EdgeInsets.all(20),
+              child: SafeArea(
+                child: Image.asset("assets/img/logo_icfes.png"),
+              ),
+            ),
+            Positioned(
+              right: 0,
+              top: 0,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(100),
+                  ),
+                ),
+                padding: EdgeInsets.only(left: 50, top: 20),
+                width: 100,
+                height: 100,
+                child: SafeArea(
+                  child: FaIcon(FontAwesomeIcons.commentDots),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
