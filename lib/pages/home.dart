21f18964c40c0ecf414f89eba4bd203789ebb03e
DuvanG <duvan.gonzalez@correo.usa.edu.co>
2020-09-11 08:53:12 -0500

    vista

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+final estiloTexto = new TextStyle(fontSize: 25);
+
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: _topBar(context),
-      body: Container(
-        child: Text("Hola mundo cruel"),
-      ),
-      //bottomNavigationBar: BottomNavigationBar(items: null),
-    );
+        appBar: _topBar(context),
+        body: Container(
+          child: Text("Hola mundo cruel", style: estiloTexto),
+        ),
+        //bottomNavigationBar: BottomNavigationBar(items: null),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: _crearBotones());
   }
 
   Widget _topBar(context) {
@@ -51,6 +54,30 @@ class HomePage extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+
+  Widget _crearBotones() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        SizedBox(
+          width: 180,
+        ),
+        FloatingActionButton(
+            backgroundColor: (Colors.pinkAccent),
+            elevation: 50.0,
+            child: Icon(
+              Icons.videocam_off,
+              color: Colors.white,
+            ),
+            onPressed: null),
+        Expanded(
+            child: SizedBox(
+          width: 20.0,
+        )),
+        //SizedBox(width: 20.0),
+      ],
     );
   }
 }
