e56b1dbe5de0a143fa03d7df2669c10760ca414f
Sergio Torres <sdtorresl@unal.edu.co>
2020-10-05 18:02:25 -0500

    Colors

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,13 @@ void main() {
 }
 
 class IcfesApp extends StatelessWidget {
-  final Color accent = Color(0xffe7326e);
-  final Color accentLight = Color(0xffff6c9c);
-  final Color accentDark = Color(0xffaf0043);
-  final Color primary = Color(0xff0074ca);
-  final Color primaryLight = Color(0xff5ba2fe);
-  final Color primaryDark = Color(0xff004999);
-  final Color grey = Color(0xffcccccc);
+  Color get accent => Color(0xffe7326e);
+  Color get accentLight => Color(0xffff6c9c);
+  Color get accentDark => Color(0xffaf0043);
+  Color get primary => Color(0xff0074ca);
+  Color get primaryLight => Color(0xff5ba2fe);
+  Color get primaryDark => Color(0xff004999);
+  Color get grey => Color(0xfff3f3f3);
 
   @override
   Widget build(BuildContext context) {
@@ -39,35 +39,35 @@ class IcfesApp extends StatelessWidget {
         ),
         textTheme: TextTheme(
           headline1: TextStyle(
-              fontSize: 16.0,
-              fontFamily: 'Poppinsmedium',
+              fontSize: 22.0,
+              fontFamily: 'PoppinsMedium',
               color: Colors.white,
               fontWeight: FontWeight.bold),
           headline2: TextStyle(
-            fontSize: 16.0,
+            fontSize: 21.0,
             fontFamily: 'PoppinsMedium',
             color: Colors.black,
             fontWeight: FontWeight.bold,
           ),
           headline3: TextStyle(
-            fontSize: 14.0,
+            fontSize: 20.0,
             fontFamily: 'Poppins',
             color: Colors.black,
           ),
           headline4: TextStyle(fontSize: 18.0, fontFamily: 'MontserratRegular'),
-          headline5: TextStyle(fontSize: 12.0, fontFamily: 'Montserrat Light'),
+          headline5: TextStyle(fontSize: 16.0, fontFamily: 'Montserrat Light'),
           bodyText1: TextStyle(
-            fontSize: 12.0,
+            fontSize: 16.0,
             fontFamily: 'MontserratLight',
             color: Colors.white,
           ),
-          bodyText2: TextStyle(fontSize: 12.0, fontFamily: 'MontserratRegular'),
+          bodyText2: TextStyle(fontSize: 16.0, fontFamily: 'MontserratRegular'),
           subtitle1: TextStyle(
             fontSize: 12.0,
             fontFamily: 'MontserratMedium',
             color: Colors.black87,
           ),
-          subtitle2: TextStyle(fontSize: 12.0, fontFamily: 'MontserratRegular'),
+          subtitle2: TextStyle(fontSize: 14.0, fontFamily: 'MontserratRegular'),
           caption: TextStyle(
             fontSize: 25.0,
             fontFamily: 'MontserratMedium',
