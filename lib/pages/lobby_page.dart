3ad5d2bb47daf4b983c1a64428d4cde47aacfa0e
Sergio Torres <sdtorresl@unal.edu.co>
2020-10-06 18:41:39 -0500

    Countdown in lobby

--- lib/pages/lobby_page.dart
+++ lib/pages/lobby_page.dart
@@ -2,6 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:icfesapp/common/countdown.dart';
 import 'package:icfesapp/common/event_description.dart';
 import 'package:icfesapp/common/expansion_card.dart';
 import 'package:icfesapp/main.dart';
@@ -36,47 +37,65 @@ class _LobbyPageState extends State<LobbyPage> {
           EventModel event = snapshot.data;
           List<SectionModel> sections = event.sections;
 
-          Widget description = Positioned(
-            top: 0,
-            left: 0,
-            right: 0,
-            child: EventDescription(
-              title: event.title,
-              description: event.description,
-              picture: event.picture.toString(),
+          print("Event: ${event.startDate}");
+
+          Widget description = Container(
+            height: 400,
+            child: Stack(
+              children: <Widget>[
+                Positioned(
+                  top: 10,
+                  left: 0,
+                  right: 0,
+                  child: EventDescription(
+                    title: event.title,
+                    description: event.description,
+                    picture: event.picture.toString(),
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: IcfesApp().grey,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(25),
+                        topRight: Radius.circular(25),
+                      ),
+                    ),
+                    child: SizedBox(
+                      height: 25,
+                    ),
+                  ),
+                )
+              ],
             ),
           );
 
-          return Stack(
+          return ListView(
             children: [
               description,
-              Positioned(
-                top: 350,
-                left: 0,
-                right: 0,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: IcfesApp().grey,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(25),
-                      topRight: Radius.circular(25),
-                    ),
-                  ),
-                  padding: EdgeInsets.all(25),
-                  child: Column(
-                    children: [
-                      Container(
-                        padding: EdgeInsets.all(10),
-                        margin: EdgeInsets.only(bottom: 20),
-                        child: Text(
-                          "Empieza nuestro conteo para el evento",
-                          style: Theme.of(context).textTheme.headline2,
-                        ),
+              Container(
+                decoration: BoxDecoration(
+                  color: IcfesApp().grey,
+                ),
+                padding: EdgeInsets.only(left: 25, right: 25, bottom: 40),
+                child: Column(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      margin: EdgeInsets.only(bottom: 20),
+                      child: Text(
+                        "Empieza nuestro conteo para el evento",
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.headline2,
                       ),
-                      //_watch(context),
-                      _sections(context, sections),
-                    ],
-                  ),
+                    ),
+                    Countdown(startDate: event.startDate),
+                    _sections(context, sections),
+                  ],
                 ),
               ),
             ],
@@ -94,9 +113,6 @@ class _LobbyPageState extends State<LobbyPage> {
   }
 
   Widget _sections(context, List<SectionModel> sections) {
-    print("Sections: $sections");
-    print("Sections: ${sections.length}");
-
     return ColumnBuilder(
       itemCount: sections.length,
       itemBuilder: (context, index) {
@@ -114,111 +130,4 @@ class _LobbyPageState extends State<LobbyPage> {
       },
     );
   }
-}
-
-Widget _watch(context) {
-  return Column(
-    children: [
-      Container(
-        child: Row(
-          children: <Widget>[
-            Positioned(
-              child: Container(
-                height: 100.0,
-                width: 120.0,
-                child: Stack(
-                  children: <Widget>[
-                    Positioned(
-                      child: Column(
-                        children: [
-                          Container(
-                            height: 70.0,
-                            width: 100.0,
-                            decoration: BoxDecoration(
-                                border:
-                                    Border.all(width: 5, color: Colors.blue),
-                                shape: BoxShape.circle,
-                                color: Colors.lightBlue[900]),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Text(
-                                  '03',
-                                  style: Theme.of(context).textTheme.caption,
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Positioned(
-              child: Container(
-                height: 100.0,
-                width: 120.0,
-                child: Stack(
-                  children: <Widget>[
-                    Positioned(
-                      child: Container(
-                        height: 70.0,
-                        width: 100.0,
-                        decoration: BoxDecoration(
-                            border: Border.all(width: 5, color: Colors.blue),
-                            shape: BoxShape.circle,
-                            color: Colors.lightBlue[900]),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Text(
-                              '72',
-                              style: Theme.of(context).textTheme.caption,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Positioned(
-              child: Container(
-                height: 100.0,
-                width: 120.0,
-                child: Stack(children: <Widget>[
-                  Positioned(
-                    child: Container(
-                      height: 70.0,
-                      width: 100.0,
-                      decoration: BoxDecoration(
-                        border: Border.all(width: 5, color: Colors.blue),
-                        shape: BoxShape.circle,
-                        color: Colors.lightBlue[900],
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Text(
-                            '00',
-                            style: Theme.of(context).textTheme.caption,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ]),
-              ),
-            ),
-          ],
-        ),
-      ),
-    ],
-  );
 }
