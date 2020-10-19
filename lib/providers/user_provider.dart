37fdecce618b8f0d946407a05f60d2de33684e08
DuvanG <duvan.gonzalez@correo.usa.edu.co>
2020-10-19 13:45:00 -0500

    funcion

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -1,27 +1,32 @@
+import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 import 'dart:convert' as json;
-
+import 'package:icfesapp/models/schedule_model.dart';
 import 'package:icfesapp/models/user_model.dart';
 
-class RoomsProvider {
-  final String _url = " https://dev-eweb.us.seedcloud.co/login";
+class UserProvider {
+  final String _url = "https://dev-eweb.us.seedcloud.co/login'";
 
-  Future<List<UserModel>> post() async {
+  Future<List<UserModel>> login() async {
     try {
       var response = await http.get(_url);
+
+      //print({responseCode: response.statusCode})
 
       if (response.statusCode == 200) {
         print(response.body);
         List<dynamic> jsonResponse = json.jsonDecode(response.body);
-        List<UserModel> token = List();
+        List<ScheduleModel> schedules = List();
 
         for (var item in jsonResponse) {
-          UserModel login = UserModel.fromMap(item);
-          token.add(login);
-          print(login);
+          print(item);
+          ScheduleModel schedule = ScheduleModel.fromMap(item);
+          schedules.add(schedule);
+          print(schedule);
         }
 
-        return token;
+        return schedules;
       } else {
         print('Request failed with status: ${response.statusCode}.');
       }
@@ -29,4 +34,25 @@ class RoomsProvider {
 
     return [];
   }
+}
+
+main() async {
+  String username = 'eweb';
+  String password = 'E02i4BMX';
+  String basicAuth =
+      'Basic ' + base64Encode(utf8.encode('$username:$password'));
+  print(basicAuth);
+
+  Response r = await get(' https://dev-eweb.us.seedcloud.co/login',
+      headers: <String, String>{'authorization': basicAuth});
+  print(r.statusCode);
+  print(r.body);
+
+  var url = 'https://dev-eweb.us.seedcloud.co/login';
+  http.post(url, body: json.encode({'email': 'code'})).then(
+    (response) {
+      print("Response status: ${response.statusCode}");
+      print("Response body: ${response.body}");
+    },
+  );
 }
